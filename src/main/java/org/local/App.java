ee800d97ed36b02ebbb4188bb34d975175321428
raadi92 <davebeltra92@gmail.com>
2024-04-02 20:44:30 +0200

    base commit

---
+++ src/main/java/org/local/App.java
@@ -0,0 +1,15 @@
+package org.local;
+
+import java.util.logging.Logger;
+/**
+ *
+ */
+public class App 
+{
+    static Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main( String[] args )
+    {
+        logger.info("Hello World!" );
+    }
+}
